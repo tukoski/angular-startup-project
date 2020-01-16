5df105e3b8483b848f20bd8444e8171606b697da
Walter <walter@fleks.info>
2020-01-16 00:10:50 -0500

    Getting started - Managin Data

---
+++ src/app/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getItems() {
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
